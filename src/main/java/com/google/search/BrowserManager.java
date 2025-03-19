3c2ac8bd9a05f15cf9677375f9a3b9919bd2ceff
y.tsuprak <y.tsuprak@pos-credit.ru>
2025-03-19 11:12:22 +0700

    Добавляем headless режим работы

--- src/main/java/com/google/search/BrowserManager.java
+++ src/main/java/com/google/search/BrowserManager.java
@@ -3,14 +3,27 @@ package com.google.search;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserManager {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
+            // Устанавливаем ChromeDriver, если он не установлен
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            // Создаём объект настроек Chrome
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless"); // Запуск в headless-режиме (обязательно для Docker)
+            options.addArguments("--no-sandbox"); // Отключаем sandbox для запуска в контейнере
+            options.addArguments("--disable-dev-shm-usage"); // Используем shared memory
+            options.addArguments("--disable-gpu"); // Отключаем GPU, так как он не нужен в headless-режиме
+            options.addArguments("--remote-allow-origins=*"); // Разрешаем удалённые соединения
+            options.addArguments("--disable-software-rasterizer"); // Отключаем software рендеринг
+            options.addArguments("--window-size=1920,1080"); // Устанавливаем размер окна браузера
+
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
